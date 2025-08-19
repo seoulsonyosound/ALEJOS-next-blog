7795d171dd332d000a7bbc75bea53c6f171f784c
seoulsonyosound <alejostheeannajether@gmail.com>
2025-08-19 13:44:06 +0800

    updated description

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Theeanna Jether D. Alejos | wlw | Mad Money Club & Club Icarus!</p>
+        <p>Theeanna Jether D. Alejos | wlw | Mad Money Club & Club Icarus! | ley's</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
